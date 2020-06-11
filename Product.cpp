95689f2aa191607e855336925445608d7652fb75
Naseeha731 <66783493+Naseeha731@users.noreply.github.com>
2020-06-11 19:23:52 +0530

    created by Product.cpp
    this file print the product of two numbers

---
+++ Product.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+float a,b,c;
+cout<<"enter the first number:"<<endl;
+cin>>a;
+cout<<"enter the secong number:"<<endl;
+cin>>b;
+c=a*b;
+cout<<"product="<<c;
+return 0;
+}
