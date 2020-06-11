c40af839acfdf9c3a8ecee3dc6fd418b9e32855b
Naseeha731 <66783493+Naseeha731@users.noreply.github.com>
2020-06-11 19:05:43 +0530

    created by vote.cpp
    This file check the vote eligibility persons

---
+++ vote.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int age;
+cout<<"enter your age:";
+cin>>age;
+if(age>=13&&age<=19)
+{
+cout<<"he/she is teenager"<<endl;
+}
+else
+{
+cout<<"he/she is not a teenager<<endl;
+}
+if(age>=18)
+{
+cout<<"he/she is eligible for voting"<<endl;
+}
+else
+{
+cout<<""he/she is not eligible for voating"<<endl;
+}
+return 0;
+}
